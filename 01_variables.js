cd6ef585bce0cbf46b6439489636b43d8d9d0214
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-02-18 15:46:52 +0530

    Done with variables and datatypes

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accId = 123;
+let accEmail = "abab@gmail.com";
+accCity = "Jaipur"; //Bad Practice
+
+accEmail = "abc@gmail.com";
+let accState; //allowed
+
+//accId = 2233; //Not allowed
+
+console.table([accId, accEmail, accCity, accState]);
---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+let age = 10;
+let name = "Sameer";
+let isSignedin = false;
+let phoneNum = null;
+let state;
+
+console.table([age, name, isSignedin, phoneNum, state]);
+console.table([
+  typeof age,
+  typeof name,
+  typeof isSignedin,
+  typeof phoneNum,
+  typeof state,
+]);
