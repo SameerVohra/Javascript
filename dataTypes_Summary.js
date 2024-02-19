4985d9b4976d1cd76cbf2e1c6fbc02ea9a54e245
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-02-19 10:26:21 +0530

    done with datatypes summary

---
+++ dataTypes_Summary.js
@@ -0,0 +1,34 @@
+//NON-PREMITIVE DATATYPES
+//Arrays
+const names = ["Sameer", "Saksham", "Saiyad"];
+
+//Objects
+const myObj = {
+  name: "Sameer",
+  age: 20,
+};
+
+//Function
+const myFunc = function () {
+  console.log("Hello World");
+};
+
+//PREMITIVE DATATYPES
+
+//String
+let str1 = "Sameer";
+
+//Number
+let age = 20;
+
+//Boolean
+let isLoggedIn = false;
+
+//Symbol
+let int1 = Symbol(123);
+let int2 = Symbol(123);
+console.log(int1 === int2);
+
+//BigInt
+let BigInt = 39840940974309;
+console.log(BigInt);
