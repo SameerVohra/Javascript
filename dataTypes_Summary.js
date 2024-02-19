c84b62d045795dffca8c793c8be5bf89071043e1
SameerVohra <sameer6002.be21@chitkara.edu.in>
2024-02-19 10:54:02 +0530

    Done with types of memory

--- dataTypes_Summary.js
+++ dataTypes_Summary.js
@@ -10,7 +10,7 @@ const myObj = {
 
 //Function
 const myFunc = function () {
-  console.log("Hello World");
+  //console.log("Hello World");
 };
 
 //PREMITIVE DATATYPES
@@ -27,8 +27,29 @@ let isLoggedIn = false;
 //Symbol
 let int1 = Symbol(123);
 let int2 = Symbol(123);
-console.log(int1 === int2);
+//console.log(int1 === int2);
 
 //BigInt
 let BigInt = 39840940974309;
-console.log(BigInt);
+//console.log(BigInt);
+
+// ***********************************MEMORY********************************
+
+let name1 = "Dhruv";
+let name2 = name1;
+
+name2 = "Sameer";
+
+console.log(name1);
+console.log(name2);
+
+let user1 = {
+  email: "user@gmail.com",
+  upiId: "user@hdfc",
+};
+
+let user2 = user1;
+user2.email = "sameer@gmail.com";
+
+console.log(user1.email);
+console.log(user2.email);
